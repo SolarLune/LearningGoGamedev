3bd76c590df00e62dfdfe416a9a0f31c4f0d8f34
SolarLune <solarlune@gmail.com>
2018-08-12 17:07:24 -0700

    Now we can exit with escape

--- src/main.go
+++ src/main.go
@@ -1,18 +1,29 @@
 package main
 
 import (
+	"errors"
 	"log"
 
 	"github.com/hajimehoshi/ebiten"
 )
 
+var screenW = 320
+var screenH = 180
+
 func update(screen *ebiten.Image) error {
-	return nil
+
+	var finishedState error
+
+	if ebiten.IsKeyPressed(ebiten.KeyEscape) {
+		finishedState = errors.New("Exit")
+	}
+
+	return finishedState
 }
 
 func main() {
 
-	if err := ebiten.Run(update, 320, 240, 2, "Test"); err != nil {
+	if err := ebiten.Run(update, screenW, screenH, 4, "Learning Go Gamedev"); err != nil {
 		log.Fatal(err)
 	}
 
